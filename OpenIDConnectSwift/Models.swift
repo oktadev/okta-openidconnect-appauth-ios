fbd13eeaa4b1683b3f3805397cae940f664c4d4e
jmelberg <jordan.melberg@okta.com>
2017-12-04 10:57:31 -0800

    :fire: Updates repo location

--- OpenIDConnectSwift/Models.swift
+++ OpenIDConnectSwift/Models.swift
@@ -28,7 +28,7 @@ class OktaConfiguration {
         kIssuer = "https://example.oktapreview.com"                        // Base url of Okta Developer domain
         kClientID = "applicationClientId"                                  // Client ID of Application
         apiEndpoint = URL(string: "https://example.com/protected")         // Resource Server URL
-        kRedirectURI = "com.okta.applicationclientid:/callback"
+        kRedirectURI = "com.oktapreview.{yourOrg}:/callback"
         kAppAuthExampleAuthStateKey = "com.okta.openid.authState"
     }
 }
